6dd8ae34e906e378b41671599fb39e1343426e6d
Samuel Prado <samuelsantanaprado@gmail.com>
2026-02-03 13:06:19 -0300

    Estrutura de mapeamento de locais integrados

---
+++ app/config/mapeamento.py
@@ -0,0 +1,10 @@
+LOCAIS = {
+    "Local 1": {
+        "app_key": "123456",
+        "app_secret": "123456"
+    },
+    "Local 2": {
+        "app_key": "654321",
+        "app_secret": "654321"
+    }
+}
