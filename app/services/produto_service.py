b6a30e77086492f03195209b8bb873fd1cb3a4b8
Samuel Prado <samuelsantanaprado@gmail.com>
2026-02-03 13:20:54 -0300

    função para listar produtos

---
+++ app/services/produto_service.py
@@ -0,0 +1,33 @@
+import time
+import logging
+from .omie_client import post_omie
+
+logger = logging.getLogger("associacoes")
+
+def listar_produtos(app_key, app_secret):
+    pagina = 1
+    produtos = []
+
+    while True:
+        payload = {
+            "call": "ListarProdutosResumido",
+            "app_key": app_key,
+            "app_secret": app_secret,
+            "param": [{
+                "pagina": pagina,
+                "registros_por_pagina": 100,
+                "apenas_importado_api": "N",
+                "filtrar_apenas_omiepdv": "N"
+            }]
+        }
+
+        response = post_omie(payload)
+
+        produtos.extend(response.get("produto_servico_resumido", []))
+
+        if pagina >= response.get("total_de_paginas", 1):
+            break
+
+        pagina += 1
+
+    return produtos
